fe9b68d73e52280ea58413dfae68345f7a96fac7
DanVerh <danverh29@gmail.com>
2023-10-19 02:15:15 +0300

    history option added

--- lab1/history.py
+++ lab1/history.py
@@ -4,6 +4,7 @@ from collections import OrderedDict
 
 historyDict = OrderedDict()
 
+
 def addResult(num1, operator, num2):
     if operator != "sqrt":
         problem = num1 + operator + num2
@@ -13,7 +14,15 @@ def addResult(num1, operator, num2):
         result = calculate(num1, operator)
     historyDict[problem] = result
 
+
 def printHistory():
     for index, problem in enumerate(historyDict):
-        index+=1
+        index += 1
         print(f"{index}. {problem} = {historyDict[problem]}")
+
+
+def getResultByIndex(historyIndex):
+    for index, problem in enumerate(historyDict):
+        if index == historyIndex:
+            print("num1 =", historyDict[problem])
+            return historyDict[problem]
--- lab1/main.py
+++ lab1/main.py
@@ -3,9 +3,19 @@ from input import *
 from history import *
 
 close = ""
+count = 0
 
 while close != "n":
-    num1 = inputNumber()
+    if count == 0:
+        num1 = inputNumber()
+    else:
+        option = "-1"
+        while not 0 <= int(option) <= 10:
+            option = input("Input manually num1 - 0, take from history - 1-10: ")
+        if option == "0":
+            num1 = inputNumber()
+        else:
+            num1 = str(getResultByIndex(int(option)-1))
     operator = inputOperator()
     if operator != "sqrt":
         num2 = inputNumber()
@@ -14,13 +24,16 @@ while close != "n":
 
     print(calculate(num1, operator, num2))
     addResult(num1, operator, num2)
+    count += 1
     close = input("Do you want to continue or see history? (y/n/h): ")
     while close not in ("y", "n"):
         if close == "n":
             print("Exiting the program")
             break
-        elif close not in ("y", "n", "h"):
-            close = input("Please provide the correct input (y/n/h): ")
+        elif close == "y":
+            break
         elif close == "h":
             printHistory()
             close = input("Do you want to continue? (y/n): ")
+        else:
+            close = input("Please provide the correct input (y/n/h): ")
