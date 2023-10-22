cdd8c1b5ace7dae109ae289d28a2895167d219a9
DanVerh <danverh29@gmail.com>
2023-10-22 20:12:53 +0300

    Error handling added

--- lab2/interface.py
+++ lab2/interface.py
@@ -8,7 +8,15 @@ class Interface:
     def __init__(self):
         self.option = 0
         while int(self.option) < 1 or int(self.option) > 3:
-            self.option = input("Please select the option (1 - Calculator, 2 - See history, 3 - Exit): ")
+            try:
+                self.option = input("Please select the option (1 - Calculator, 2 - See history, 3 - Exit): ")
+                if not str(self.option).isdigit():
+                    self.option = 0
+                    raise InputException("EXCEPTION: INPUT CAN ONLY BE 1, 2 or 3")
+                elif int(self.option) < 1 or int(self.option) > 3:
+                    raise InputException("EXCEPTION: INPUT CAN ONLY BE 1, 2 or 3")
+            except InputException as error:
+                print(error)
 
     def start(self):
         if self.option == "1":
