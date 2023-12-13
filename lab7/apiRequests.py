42d0910ac4d3d3192c8bad9749809286035ad6db
DanVerh <danverh29@gmail.com>
2023-12-13 22:35:29 +0200

    date formatting added

--- lab7/apiRequests.py
+++ lab7/apiRequests.py
@@ -1,6 +1,9 @@
 import requests
 from prettytable import PrettyTable
+from datetime import datetime
 
+class InputException(Exception):
+    pass
 
 class Request:
     def __init__(self):
@@ -28,9 +31,20 @@ class Request:
         else:
             users = self.getUsers()
             for user in users:
+                try:
+                    if user.get('birthDate', 'NULL') != "NULL":
+                        bithdate = datetime.strptime(user.get('birthDate', 'NULL'), "%Y-%m-%d")
+                    else:
+                        raise InputException("EXCEPTION: DATE FORMAT IS INVALID")
+                except InputException as error:
+                    print(error)
+                    break
+                formattedBirthdate = bithdate.strftime("%d %B %Y")
                 print("firstname : {}".format(user.get('firstname', 'NULL')))
                 print("lastname  : {}".format(user.get('lastname', 'NULL')))
                 print("email     : {}".format(user.get('email', 'NULL')))
+                print("username  : {}".format(user.get('login', {}).get('username', 'NULL')))
+                print("birthdate : {}".format(formattedBirthdate))
                 print()
 
     def printUsersTable(self):
@@ -38,12 +52,25 @@ class Request:
             print("Error getting the result")
         else:
             users = self.getUsers()
-            table = PrettyTable(['Firstname', 'Lastname', 'Email'])
+            table = PrettyTable(['Firstname', 'Lastname', 'Email', 'Username', 'Birthdate'])
             for user in users:
+                try:
+                    if user.get('birthDate', 'NULL') != "NULL":
+                        bithdate = datetime.strptime(user.get('birthDate', 'NULL'), "%Y-%m-%d")
+                    else:
+                        raise InputException("EXCEPTION: DATE FORMAT IS INVALID")
+                except InputException as error:
+                    print(error)
+                    break
+                formattedBirthdate = bithdate.strftime("%d %B %Y")
                 table.add_row([user.get('firstname', 'N/A'),
                                user.get('lastname', 'N/A'),
-                               user.get('email', 'N/A')])
-        print(table)
+                               user.get('email', 'N/A'),
+                               user.get('login', {}).get('username', 'N/A'),
+                               formattedBirthdate])
+            print(table)
+
+    #def printOneUser(self):
 
 
 request = Request()
---
+++ lab7/dateMonthList.py
@@ -0,0 +1,4 @@
+month_list = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+]
